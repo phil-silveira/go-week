abef0e914cf8b35f4bb82b9cd686bb84abb567f2
Phil <--->
2018-11-08 00:05:46 -0200

    fix(frontend): implementação completa

--- frontend/src/pages/Timeline.jsx
+++ frontend/src/pages/Timeline.jsx
@@ -1,23 +1,59 @@
 import React from 'react'
+import socket from 'socket.io-client'
+
+import api from '../services/api'
+
+import Tweet from '../components/Tweet'
 
 import twitterLogo from '../twitter.svg'
 import './Timeline.css'
 
 export default class Timeline extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {}
+    state = {
+        newTweet: '',
+        tweets: []
     }
 
     handleInputChange = (e) => {
         this.setState({ newTweet: e.target.value })
     }
 
-    handleNewTweet = (e) => {
+    handleNewTweet = async (e) => {
         if (e.keyCode !== 13) return
 
         const content = this.state.newTweet;
         const author = localStorage.getItem('@GoTwitter')
+
+        await api.post('/tweets', { author, content })
+
+        this.setState({ newTweet: '' })
+    }
+
+    subscribeToEvents = () => {
+        const io = socket('http://localhost:3000')
+
+        io.on('tweet', data => {
+            console.log(data)
+
+            this.setState({ tweets: [data, ...this.state.tweets] })
+        })
+        io.on('like', data => {
+            this.setState({
+                tweets: this.state.tweets.map(tweet => {
+                    return tweet._id === data._id ? data : tweet
+                })
+            })
+
+            console.log(data)
+        })
+    }
+
+    async componentDidMount() {
+        this.subscribeToEvents()
+
+        const response = await api.get('/tweets')
+
+        this.setState({ tweets: response.data })
     }
 
     render() {
@@ -33,8 +69,11 @@ export default class Timeline extends React.Component {
                         placeholder='O que esta acontecendo?'
                     ></textarea>
                 </form>
-
-                {/* <ul></ul> */}
+                <ul className='tweet-list'>
+                    {this.state.tweets.map(tweet => {
+                        return <Tweet key={tweet._id} tweet={tweet} />
+                    })}
+                </ul>
             </div>
         )
     }
